3f843e5da92bd0cf61514b84aeada5fc2774e83f
warren <warrendlee1@gmail.com>
2022-09-23 21:39:56 -0700

    confirmation page WIP

--- src/pages/Confirmation/index.jsx
+++ src/pages/Confirmation/index.jsx
@@ -1,13 +1,27 @@
+// Libraries
 import React from "react";
 import { Link } from "react-router-dom";
+
+// Files
+import "./index.css";
 
 const Confirmation = (props) => {
   const { content } = props;
   return (
-    <div>
-      <p>{content["primary-message"]}</p>
-      <p>{content["secondary-message"]}</p>
-      <Link to="/">{content["submit-button"]}</Link>
+    <div className="confirmation-root">
+      <div className="confirmation-content">
+        <div className="confirmation-body">
+          <p className="confirmation-title">{content["primary-message"]}</p>
+          <p className="confirmation-subtitle">
+            {content["secondary-message"]}
+          </p>
+        </div>
+        <div className="confirmation-footer">
+          <Link className="confirmation-button" to="/">
+            {content["submit-button"]}
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
