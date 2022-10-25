43ffd6d09279a6c32a4690c5a27beca0579b7062
Harrison Roskopp <hrroskopp@gmail.com>
2022-10-25 14:26:32 -0700

    Add firebase config and auth hook

--- src/geomath.test.js
+++ src/geomath.test.js
@@ -10,7 +10,7 @@ test("Only obstructions within max distance are returned", () => {
     { latitude: 33.98194710422966, longitude: -118.43807774028127 },
     { latitude: 34.01077694661349, longitude: -118.48492649262909 },
   ];
-  const maxDistance = 4828; //In meter. Approx 3 miles.
+  const maxDistance = 4828; // In meters. Approx 3 miles.
   expect(getObstructionsNearby(location, obstructions, maxDistance)).toEqual([
     { latitude: 33.9829858979619, longitude: -118.40694191685019 },
     { latitude: 33.98194710422966, longitude: -118.43807774028127 },
--- src/hooks/useGeoData.js
+++ src/hooks/useGeoData.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export default function useGeoData() {
+export function useGeoData() {
   const [geoData, setGeoData] = useState({});
   const [error, setError] = useState(null);
 
